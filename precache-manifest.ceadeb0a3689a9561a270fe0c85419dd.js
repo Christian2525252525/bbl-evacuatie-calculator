ea9f8600f2c923386d9154b9f8fb1e6bd7a9d4ac
Christian de Keijzer <49040752+Christian2525252525@users.noreply.github.com>
2025-03-02 17:14:31 +0100

    Updates

---
+++ precache-manifest.ceadeb0a3689a9561a270fe0c85419dd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba6ed0c4488294bd802d022af8a4f554",
+    "url": "/bbl-evacuatie-calculator/index.html"
+  },
+  {
+    "revision": "752f96f14ab52da7e59a",
+    "url": "/bbl-evacuatie-calculator/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "9c702204bcc5bf90bca5",
+    "url": "/bbl-evacuatie-calculator/static/js/2.09b2c203.chunk.js"
+  },
+  {
+    "revision": "0938d737fd4884568206",
+    "url": "/bbl-evacuatie-calculator/static/js/3.fdbdb794.chunk.js"
+  },
+  {
+    "revision": "752f96f14ab52da7e59a",
+    "url": "/bbl-evacuatie-calculator/static/js/main.236b8286.chunk.js"
+  },
+  {
+    "revision": "4fcfc5c343b12347822a",
+    "url": "/bbl-evacuatie-calculator/static/js/runtime~main.155f4cb3.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/bbl-evacuatie-calculator/precache-manifest.ceadeb0a3689a9561a270fe0c85419dd.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/bbl-evacuatie-calculator/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
